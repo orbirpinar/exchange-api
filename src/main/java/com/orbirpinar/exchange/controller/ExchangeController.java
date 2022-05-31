3e24aee34f8ace414ba24855b58a2b7d5418bf5b
orbirpinar <orbirpinar@gmail.com>
2022-06-01 00:48:32 +0300

    Add detailed openapi-docs and new controller for transactionId

--- src/main/java/com/orbirpinar/exchange/controller/ExchangeController.java
+++ src/main/java/com/orbirpinar/exchange/controller/ExchangeController.java
@@ -2,14 +2,26 @@ package com.orbirpinar.exchange.controller;
 
 import com.orbirpinar.exchange.dto.request.ExchangeConverterRequest;
 import com.orbirpinar.exchange.dto.request.ExchangeRateRequest;
+import com.orbirpinar.exchange.dto.response.ExchangeConverterResponse;
+import com.orbirpinar.exchange.dto.response.ExchangeRateResponse;
+import com.orbirpinar.exchange.exception.ApiError;
 import com.orbirpinar.exchange.service.ExchangeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Positive;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -22,27 +34,71 @@ public class ExchangeController {
         this.exchangeService = exchangeService;
     }
 
+    @Operation(summary = "Get exchange rate from external api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Getting to exchange rate",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ExchangeRateResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "currency is not valid",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "target,source can't be null",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))})
+    })
     @PostMapping(value = "/rate")
     public ResponseEntity<?> getExchangeRate(@RequestBody @Valid ExchangeRateRequest request) {
         return ResponseEntity.ok(exchangeService.getExchangeRate(request));
     }
 
+    @Operation(summary = "Convert amount of currency to another currency and persist to database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Getting to target amount",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ExchangeConverterResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "currency is not valid",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "target,source and amount can't be null",
+                    content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ApiError.class))})
+    })
     @PostMapping(value = "/conversion")
     public ResponseEntity<?> convert(@RequestBody @Valid ExchangeConverterRequest request) {
         return ResponseEntity.ok(exchangeService.convert(request));
     }
 
+    @Operation(summary = "Get all conversions by filters with pagination",
+    description =
+            "** If no parameter given then all conversion will be listed with pagination <br />" +
+            "** If fromDate and toDate parameter given then all conversion will be listed between two dates with pagination <br /> " +
+            "** If you want to filter by date then fromDate and toDate both must be specified <br />" +
+            "** If pageNumber and pageSize not specified pageSize' s default value 5 and pageNumber' s default value 0")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of conversion",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ExchangeConverterResponse.class))) }),
+            @ApiResponse(responseCode = "400", description = "pageSize and pageNumber can't be negative",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))})
+    })
     @GetMapping(value = "/conversion")
-    public ResponseEntity<?> getAllConversion(
+    public ResponseEntity<Page<ExchangeConverterResponse>> getAllConversion(
             @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
            @RequestParam(value = "pageNumber",  defaultValue = "0") Integer pageNumber
     ) {
         return ResponseEntity.ok(exchangeService.getAllConversions(pageNumber, pageSize));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "fromDate and toDate should be valid format see example",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))})
+    })
     @GetMapping(value = "/conversion", params = {"fromDate", "toDate"})
     public ResponseEntity<?> getAllConversionsBetweenTransactionDates(
+            @Parameter(example = "02/06/2022", description = "Get conversions between two dates")
             @RequestParam(value = "fromDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
+            @Parameter(example = "03/06/2022", description = "Get conversions between two dates")
             @RequestParam(value = "toDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate,
             @RequestParam(value = "pageSize",  defaultValue = "5") Integer pageSize,
             @RequestParam(value = "pageNumber",  defaultValue = "0") Integer pageNumber
@@ -51,10 +107,18 @@ public class ExchangeController {
                 pageNumber, pageSize));
     }
 
-    @GetMapping(value = "/conversion", params = {"transactionId"})
-    public ResponseEntity<?> getAllConversionsById(
-            @RequestParam(value = "transactionId", required = false) String transactionId
-    ) {
+
+    @Operation(summary = "Get single conversion by transactionId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get single conversion by transactionId",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ExchangeConverterResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Resource Not Found with {transactionId}",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))})
+    })
+    @GetMapping(value = "/conversion/{transactionId}")
+    public ResponseEntity<?> getByConversionsById(@PathVariable String transactionId) {
         return ResponseEntity.ok(exchangeService.getConversionByTransactionId(transactionId));
     }
 
