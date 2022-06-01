6dbc4514898ab165dbec0cecaede21e4cdee5b6a
orbirpinar <orbirpinar@gmail.com>
2022-06-01 22:57:36 +0300

    Optimized import

--- src/main/java/com/orbirpinar/exchange/aspect/LogAspect.java
+++ src/main/java/com/orbirpinar/exchange/aspect/LogAspect.java
@@ -1,7 +1,6 @@
 package com.orbirpinar.exchange.aspect;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -20,29 +19,24 @@ import java.util.stream.Collectors;
 @Slf4j
 public class LogAspect {
 
-    
-    private final ObjectMapper mapper;
     private static final String CONTROLLER_POINTCUT = "within(com.orbirpinar.exchange.controller.*)";
 
-    public LogAspect(ObjectMapper mapper) {
-        this.mapper = mapper;
-    }
 
     @Around(CONTROLLER_POINTCUT)
     @SneakyThrows
     public Object logAround(ProceedingJoinPoint joinPoint) {
-        log.info("before {}",getLogMessage(joinPoint));
+        log.info("before {}", getLogMessage(joinPoint));
         Object proceed = joinPoint.proceed();
-        log.info("after {} : with result: {}",getLogMessage(joinPoint),proceed.toString());
+        log.info("after {} : with result: {}", getLogMessage(joinPoint), proceed.toString());
         return proceed;
     }
+
     @AfterThrowing(pointcut = CONTROLLER_POINTCUT, throwing = "e")
-    @SneakyThrows
-    public void logAfterException(JoinPoint joinPoint, Exception e) {
-        log.error("Exception during: {} with ex: {}", getLogMessage(joinPoint),  e.toString());
+    public void logAfterException(JoinPoint joinPoint, Exception e)  {
+        log.error("Exception during: {} with ex: {}", getLogMessage(joinPoint), e.toString());
     }
 
-    private String  getLogMessage(JoinPoint joinPoint) throws JsonProcessingException {
+    private String getLogMessage(JoinPoint joinPoint) {
         String args = Arrays.stream(joinPoint.getArgs())
                 .map(String::valueOf).collect(Collectors.joining(",", "[", "]"));
 
